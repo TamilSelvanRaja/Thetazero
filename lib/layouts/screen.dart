4ad73cc88a6516d0c876b28ad55bf1ded25916ee
Tamilselvan Raja <tamilselvan1998.pdkt@gmail.com>
2024-08-16 22:33:40 +0530

    file changes

--- lib/layouts/screen.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Screen {
-  static Size size(BuildContext c) => MediaQuery.of(c).size; //Device Size
-  static double width(BuildContext c) => size(c).width; // Device width
-  static double height(BuildContext c) => size(c).height; // Device height
-}
